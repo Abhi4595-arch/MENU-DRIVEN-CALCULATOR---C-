6be3909fb82099771d7c6c805327142e96a8fdc8
Abhi4595-arch <gapk4595@gmail.com>
2026-09-25 11:11:43 +0530

    Improve server socket portability

--- backend/include/api/Server.hpp
+++ backend/include/api/Server.hpp
@@ -1,7 +1,7 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
-#include <winsock2.h>
+#include <cstdint>
 
 class Router;
 
@@ -18,7 +18,7 @@ private:
     Router& router;
 
     bool running;
-    SOCKET serverSocket;
+    std::intptr_t serverSocket;
 
     void acceptConnections();
 };
